12713fbc0ecdefeb71c68ccf7f4d5649da2cce82
DanielMeij <dmeijueiro@student.alamo.edu>
2026-09-23 17:36:14 -0500

    Added Guessing Games Lua

---
+++ Guessing Game.lua
@@ -0,0 +1,38 @@
+Guess = 7
+continue = "y"
+while continue == "y" do
+    number = math.random(1, 100)
+    print("Im thinking of a number between 1 and 100")
+    NumberAnswer = tonumber(io.read())
+    while Guess > 0 do
+        if NumberAnswer == number then
+            print ("Correct! The number was " .. number)
+            Guess = 0
+        else
+            Guess = Guess - 1
+            print("Incorrect!")
+            if Guess ~= 0 then
+                if NumberAnswer < number then
+                    print ("The number is Higher")
+                else
+                    print ("The number is lower")
+                end
+                print("You have " .. Guess .. " Tries Left!")
+                NumberAnswer = tonumber(io.read())
+            else
+                print("The Number was " .. number)
+            end
+        end
+    end
+    print("Do you want to play again? y/n")
+    continue = io.read()
+    while continue ~= "y" and continue ~= "n" do
+        print("Error: Please select y or n")
+        continue = io.read()
+    end
+    if continue == "y" then
+        Guess = 7
+    else
+        print("Thank You for playing")
+    end
+end
